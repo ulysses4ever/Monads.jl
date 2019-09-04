802ef4ec8a39447fe27e4614c47c74b890fef91a
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2019-09-04 15:46:10 -0400

    remove obsolete test with promises

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,27 +20,3 @@ using Promisables
 
 end
 
-@testset "A promise monad" begin
-  @testset "must properly resolve and return a working promise" begin
-    p0 = Promise()
-    step1::Function = (x) -> Resolve(x+2);
-    step2::Function = (x) -> Resolve(x+2);
-
-    p = (@mdo Promise begin
-      x <- p0
-      y <- (step1(x))
-      z <- (step2(y))
-      return y + z
-    end)
-
-    Timer((t) -> begin
-      Fulfill(p0, 0)
-      close(t)
-    end, 2)
-
-    @pawait p
-    @test p.value == 6
-  end
-end
-
-end
